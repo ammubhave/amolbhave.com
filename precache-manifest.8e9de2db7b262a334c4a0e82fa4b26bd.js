21d5ed4c627db91cbfc488b7dc8c67b4e540ecfe
Amol Bhave <amol@amolbhave.com>
2020-04-23 21:32:57 -0400

    Updates

---
+++ precache-manifest.8e9de2db7b262a334c4a0e82fa4b26bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57df08f40615d7d496fc8dade13ab2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c05be30b8f42f8315ace",
+    "url": "/static/css/main.e7281e64.chunk.css"
+  },
+  {
+    "revision": "d73e1d2f9adb8ce81b01",
+    "url": "/static/js/2.cc24aba1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc24aba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05be30b8f42f8315ace",
+    "url": "/static/js/main.4dbe71a4.chunk.js"
+  },
+  {
+    "revision": "44243a2e67962dcb43b5",
+    "url": "/static/js/runtime-main.24d05a3e.js"
+  },
+  {
+    "revision": "a16d838ff36db42f9c5d8ee45ffbce94",
+    "url": "/static/media/amol.a16d838f.jpg"
+  }
+]);
